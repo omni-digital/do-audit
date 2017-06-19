e3a0cddf5d2b0ae237ed5d7edb59e676753bf615
Paweł Adamczak <pawel.ad@gmail.com>
2017-06-19 15:40:59 +0100

    Updated 'test_utils.py' file

--- do_audit/test/test_utils.py
+++ do_audit/test/test_utils.py
@@ -9,7 +9,7 @@ from uuid import uuid4
 import click
 import pytest
 
-from do_audit.utils import click_echo_kvp
+from do_audit import utils
 
 
 @pytest.mark.parametrize('key,value,padding,color', [
@@ -18,10 +18,12 @@ from do_audit.utils import click_echo_kvp
 ])
 def test_click_echo_kvp(key, value, padding, color, mocker):
     """
-    Test do_audit.utils.click_echo_kvp
+    Test 'do_audit.utils.click_echo_kvp'
     """
     click_echo = mocker.patch('click.echo')
-    click_echo_kvp(key, value, padding=padding, color=color)
+
+    utils.click_echo_kvp(key, value, padding=padding, color=color)
+
     click_echo.assert_called_once_with(
         click.style('{key:<{padding}}'.format(
             key=key + ':',
@@ -31,8 +33,10 @@ def test_click_echo_kvp(key, value, padding, color, mocker):
     )
 
     # Check default padding and color values
-    click_echo = mocker.patch('click.echo')
-    click_echo_kvp(key, value)
+    click_echo = mocker.patch('click.echo'
+                              )
+    utils.click_echo_kvp(key, value)
+
     click_echo.assert_called_once_with(
         click.style('{key:<{padding}}'.format(
             key=key + ':',
@@ -40,3 +44,19 @@ def test_click_echo_kvp(key, value, padding, color, mocker):
         ), fg='green') +
         str(value)
     )
+
+
+def test_yes_no():
+    """
+    Test 'do_audit.utils.yes_no'
+    """
+    assert utils.yes_no(True) == 'Yes'
+    assert utils.yes_no(False) == 'No'
+
+
+def test_droplet_url():
+    """
+    Test 'do_audit.utils.droplet_url'
+    """
+    uuid = str(uuid4())
+    assert utils.droplet_url(uuid) == 'https://cloud.digitalocean.com/droplets/{}/graphs'.format(uuid)
